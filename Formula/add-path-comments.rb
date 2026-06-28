40cfbac7eeb81247924bc2a92590af2fa05397f1
Siavosh Zarrasvand <siavosh@zarrasvand.com>
2026-06-28 16:35:27 +0300

    Release v1.0.5: update formula url, sha256, version

--- Formula/add-path-comments.rb
+++ Formula/add-path-comments.rb
@@ -2,10 +2,10 @@
 class AddPathComments < Formula
   desc "Add file path comments to TypeScript/TSX, Rust, and Python projects"
   homepage "https://github.com/SiavoshZarrasvand/homebrew-add-path-comments"
-  url "https://github.com/SiavoshZarrasvand/homebrew-add-path-comments/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "d2a17fafd35dfec6b4b71ce1ffc109be68c44943ac98ed130bd960d34f4ef454"
+  url "https://github.com/SiavoshZarrasvand/homebrew-add-path-comments/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "cadef61422c17b0bca37d987faf177d757c2637c4a1543cd28b498798dc8d058"
   license "MIT"
-  version "1.0.4"
+  version "1.0.5"
 
   depends_on :macos
 
